f7071b4a667dd4f33f9df03c960c3e12d3da1ab8
pipoon21 <pipoon22@hotmail.com>
2025-04-29 18:46:26 +0700

    add unit test for GetAccessTokenAsync method

--- GPoon21.KAPI.SDK.UnitTest/KAPITest.cs
+++ GPoon21.KAPI.SDK.UnitTest/KAPITest.cs
@@ -1,11 +1,25 @@
-﻿namespace GPoon21.KAPI.SDK.UnitTest;
+﻿using Xunit.Abstractions;
+
+namespace GPoon21.KAPI.SDK.UnitTest;
 
 public class KAPITest {
+    private readonly ITestOutputHelper _outputHelper;
+
+    public KAPITest(ITestOutputHelper outputHelper) {
+        _outputHelper = outputHelper;
+    }
 
 
     [Fact]
-    public void GetAccessTokenAsync_Success() {
+    public async Task GetAccessTokenAsync_Success() {
         
-        
+        string? customerId = Environment.GetEnvironmentVariable(nameof(customerId));
+        Assert.NotNull(customerId);
+        string? customerSecret = Environment.GetEnvironmentVariable(nameof(customerSecret));
+        Assert.NotNull(customerSecret);
+
+        string result = await KAPI.GetAccessTokenAsync(customerId, customerSecret);
+        _outputHelper.WriteLine(result);
+        Assert.False(string.IsNullOrWhiteSpace(result));
     }
 }
