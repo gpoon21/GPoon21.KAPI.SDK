71eadcfba2cd295013f34f653e0fa3d1b6b22b19
pipoon21 <pipoon22@hotmail.com>
2025-04-29 20:32:52 +0700

    add missing QRResponse parameters

--- GPoon21.KAPI.SDK/KAPI_RequestQR.cs
+++ GPoon21.KAPI.SDK/KAPI_RequestQR.cs
@@ -7,6 +7,21 @@ namespace GPoon21.KAPI.SDK;
 
 public static partial class KAPI {
     public class QRRequest {
+        public required string PartnerTransactionUid { get; init; }
+        public required string PartnerId { get; init; }
+        public required string PartnerSecret { get; init; }
+        public required string MerchantId { get; init; }
+        public string? TerminalId { get; init; }
+        public required string QRType { get; init; }
+        public required decimal TransactionAmount { get; init; }
+        public required string TransactionCurrencyCode { get; init; }
+        public required string Reference1 { get; init; }
+        public string? Reference2 { get; init; }
+        public string? Reference3 { get; init; }
+        public string? Reference4 { get; init; }
+    }
+
+    private class SerializableQRRequest {
         [JsonPropertyName("partnerTxnUid")]
         public required string PartnerTransactionUid { get; init; }
 
@@ -42,26 +57,55 @@ public static partial class KAPI {
 
         [JsonPropertyName("reference4")]
         public string? Reference4 { get; init; }
+
+        [JsonPropertyName("requestDt")]
+        public required string RequestDateTime { get; init; }
+
+        public static SerializableQRRequest FromRequest(QRRequest request) {
+            return new SerializableQRRequest {
+                PartnerTransactionUid = request.PartnerTransactionUid,
+                PartnerId = request.PartnerId,
+                PartnerSecret = request.PartnerSecret,
+                MerchantId = request.MerchantId,
+                TerminalId = request.TerminalId,
+                QRType = request.QRType,
+                TransactionAmount = request.TransactionAmount,
+                TransactionCurrencyCode = request.TransactionCurrencyCode,
+                Reference1 = request.Reference1,
+                Reference2 = request.Reference2,
+                Reference3 = request.Reference3,
+                Reference4 = request.Reference4,
+                RequestDateTime = DateTime.UtcNow.ToString("o")
+            };
+        }
     }
 
     public class QRResponse {
         [JsonPropertyName("partnerTxnUid")]
         public required string PartnerTransactionUid { get; init; }
 
+        [JsonPropertyName("partnerId")]
+        public required string PartnerId { get; init; }
+
+        [JsonPropertyName("statusCode")]
+        public required string StatusCode { get; init; }
+
+        [JsonPropertyName("errorCode")]
+        public string? ErrorCode { get; init; }
+
+        [JsonPropertyName("errorDesc")]
+        public string? ErrorDescription { get; init; }
+
+        [JsonPropertyName("accountName")]
+        public required string AccountName { get; init; }
+
         [JsonPropertyName("qrCode")]
         public required string QRCode { get; init; }
 
-        [JsonPropertyName("qrImage")]
-        public required string QRImage { get; init; }
+        [JsonPropertyName("sof")]
+        public required string[] SourceOfFunds { get; init; }
     }
 
-    /// <summary>
-    /// Requests a QR code for payment from K-Bank API
-    /// API documentation: https://apiportal.kasikornbank.com/product/public/All/QR%20Payment/Documentation/Request%20QR
-    /// </summary>
-    /// <param name="request">The QR request details</param>
-    /// <param name="accessToken">The OAuth access token obtained from GetClientCredentials</param>
-    /// <returns>QR response containing the QR code and image</returns>
     public static async Task<QRResponse> RequestQR(
         QRRequest request,
         string accessToken,
@@ -75,8 +119,11 @@ public static partial class KAPI {
         headerModifier ??= new IHeaderModifier.Default();
         headerModifier.Modify(httpRequest.Headers);
 
+        // Convert to serializable request and add timestamp
+        var serializableRequest = SerializableQRRequest.FromRequest(request);
+
         // Set JSON body
-        string jsonContent = JsonSerializer.Serialize(request);
+        string jsonContent = JsonSerializer.Serialize(serializableRequest);
         httpRequest.Content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
 
         // Send request
