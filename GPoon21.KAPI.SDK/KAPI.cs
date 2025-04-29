f2c970aada13c6bb3cad5ef6b4c00f3d21ed43be
pipoon21 <pipoon22@hotmail.com>
2025-04-29 20:07:26 +0700

    add IHeaderModifier interface

---
+++ GPoon21.KAPI.SDK/KAPI.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http.Headers;
+
+namespace GPoon21.KAPI.SDK;
+
+public static partial class KAPI {
+    public interface IHeaderModifier {
+
+        public void Modify(HttpRequestHeaders headers);
+
+        public class Test : IHeaderModifier {
+            private readonly string _envId;
+
+            public Test(string envId) {
+                _envId = envId;
+            }
+
+            public void Modify(HttpRequestHeaders headers) {
+                headers.Add("x-test-mode", "true");
+                headers.Add("env-id", _envId);
+            }
+        }
+
+        public class Default : IHeaderModifier {
+            public void Modify(HttpRequestHeaders headers) { }
+        }
+
+    }
+}
